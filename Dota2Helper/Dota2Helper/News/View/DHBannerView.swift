8c6e8ca235f842180057b9cc4dc052efad881827
Shvier <daulvi@hotmail.com>
2016-09-22 00:41:06 +0800

    Update BannerView

--- Dota2Helper/Dota2Helper/News/View/DHBannerView.swift
+++ Dota2Helper/Dota2Helper/News/View/DHBannerView.swift
@@ -9,10 +9,11 @@
 import UIKit
 import Kingfisher
 
-class DHBannerView: UIView {
+class DHBannerView: UIView, UIScrollViewDelegate {
     
     var layout: CGRect?
     var scrollView: UIScrollView?
+    var bannerCount: Int?
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -22,8 +23,20 @@ class DHBannerView: UIView {
         super.init(coder: aDecoder)
     }
     
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        let offset: CGPoint = scrollView.contentOffset
+        if offset.x > kBannerWidth*CGFloat(bannerCount!-1) {
+            scrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: false)
+        }
+        if offset.x < kBannerWidth {
+            scrollView.setContentOffset(CGPoint(x: kBannerWidth*CGFloat(bannerCount!), y: 0), animated: false)
+        }
+    }
+    
     func bindDataWithViewModel(_ viewModel: DHNewsBannerViewModel) {
+        bannerCount = viewModel.bannerCount
         scrollView = viewModel.scrollView
+        scrollView?.delegate = self
         self.addSubview(scrollView!)
     }
     
--- Dota2Helper/Dota2Helper/News/ViewModel/DHNewsBannerViewModel.swift
+++ Dota2Helper/Dota2Helper/News/ViewModel/DHNewsBannerViewModel.swift
@@ -15,25 +15,29 @@ let kBannerHeight: CGFloat = 200.0
 class DHNewsBannerViewModel: NSObject {
 
     var scrollView: UIScrollView?
+    var bannerCount: Int?
     
     init(_ banners: [DHNewsModel]) {
         super.init()
         
+        bannerCount = banners.count
         scrollView = UIScrollView(frame: CGRect(x: 0, y: 0, width: kBannerWidth, height: kBannerHeight))
         scrollView?.contentSize = CGSize(width: kBannerWidth*CGFloat(banners.count + 1), height: kBannerHeight)
         scrollView?.isPagingEnabled = true
         scrollView?.showsHorizontalScrollIndicator = false
-        let firstImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: kBannerWidth, height: kBannerHeight))
-        let url = URL(string: banners[0].background!)
-        firstImageView.kf_setImage(with: url)
-        scrollView?.addSubview(firstImageView)
-        let index = 1;
+        
+        var index = 0;
         for banner in banners {
             let imageView = UIImageView(frame: CGRect(x: kBannerWidth*CGFloat(index), y: 0, width: kBannerWidth, height: kBannerHeight))
             let url = URL(string: banner.background!)
             imageView.kf_setImage(with: url)
             scrollView?.addSubview(imageView)
+            index += 1
         }
+        let lastImageView = UIImageView(frame: CGRect(x: kBannerWidth*CGFloat(index), y: 0, width: kBannerWidth, height: kBannerHeight))
+        let url = URL(string: banners[0].background!)
+        lastImageView.kf_setImage(with: url)
+        scrollView?.addSubview(lastImageView)
     }
     
     func viewModel(_ banners: [DHNewsModel]) -> DHNewsBannerViewModel {
