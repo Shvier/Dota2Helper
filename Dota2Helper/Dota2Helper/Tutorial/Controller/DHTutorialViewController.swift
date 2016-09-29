4c941397805e6f0357f0f795d6c6b6d500050aee
Shvier <daulvi@hotmail.com>
2016-09-29 22:20:48 +0800

    Convert TutorialVC from UITableViewController to UIViewController

--- Dota2Helper/Dota2Helper/Tutorial/Controller/DHTutorialViewController.swift
+++ Dota2Helper/Dota2Helper/Tutorial/Controller/DHTutorialViewController.swift
@@ -8,14 +8,19 @@
 
 import UIKit
 
-class DHTutorialViewController: UITableViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+class DHTutorialViewController: UIViewController {
+    
+    func initLifeCycle() {
         view.backgroundColor = UIColor.white
         navigationController?.navigationBar.barTintColor = UIColor.black
         navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
         navigationItem.title = "掌刀"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        initLifeCycle()
     }
     
 }
