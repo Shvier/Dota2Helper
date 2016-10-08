71e2bd2054d134d68a74f4c4a73f6ba7e86dfee6
Shvier <daulvi@hotmail.com>
2016-10-08 16:21:37 -0200

    Add hintLabel for LoadingView

--- Dota2Helper/Dota2Helper/Tools/DHLoadingView.swift
+++ Dota2Helper/Dota2Helper/Tools/DHLoadingView.swift
@@ -12,6 +12,12 @@ class DHLoadingView: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
+        let hint: String = "正在加载"
+        let hintLabel: UILabel = UILabel(frame: CGRect(x: 0, y: 0, width: hint.sizeOfContent(font: UIFont(name: "Helvetica", size: 16)!).width, height: 18))
+        hintLabel.center = CGPoint(x: frame.size.width/2, y: frame.size.height/2)
+        
+        self.addSubview(hintLabel)
     }
     
     required init?(coder aDecoder: NSCoder) {
