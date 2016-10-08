73a5fc480a4bfa078d2eeb7f630d32f9e5c439b9
Shvier <Shvier@sina.com>
2016-10-08 16:09:30 +0800

    Init LoadingView

---
+++ Dota2Helper/Dota2Helper/Tools/DHLoadingView.swift
@@ -0,0 +1,21 @@
+//
+//  DHLoadingView.swift
+//  Dota2Helper
+//
+//  Created by Shvier on 08/10/2016.
+//  Copyright © 2016 Shvier. All rights reserved.
+//
+
+import UIKit
+
+class DHLoadingView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+}
