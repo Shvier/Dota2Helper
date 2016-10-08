401a58ad506fefb873067b57c2fe0cff3c6950e8
Shvier <daulvi@hotmail.com>
2016-10-08 17:40:18 -0200

    Convert VideoVC to UIViewController

--- Dota2Helper/Dota2Helper/Video/Controller/DHVideoViewController.swift
+++ Dota2Helper/Dota2Helper/Video/Controller/DHVideoViewController.swift
@@ -9,43 +9,44 @@
 import UIKit
 import MJRefresh
 
-class DHVideoViewController: UITableViewController {
+class DHVideoViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     var dataController: DHVideoDataController?
+    var tableView: UITableView?
     
     func handleVideoData() {
         DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-            self.tableView.mj_header.endRefreshing()
+            self.tableView?.mj_header.endRefreshing()
         }
         dataController = DHVideoDataController()
         dataController?.requestVideoDataWithCallback(callback: {
             self.renderTableViewCell()
-            tableView.mj_footer = MJRefreshAutoNormalFooter(refreshingBlock: {
+            tableView?.mj_footer = MJRefreshAutoNormalFooter(refreshingBlock: {
                 DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-                    self.tableView.mj_footer.endRefreshing()
+                    self.tableView?.mj_footer.endRefreshing()
                 }
                 self.dataController?.requestMoreVideo(callback: {
-                    self.tableView.mj_footer.endRefreshing()
+                    self.tableView?.mj_footer.endRefreshing()
                     DispatchQueue.main.async(execute: {
-                        self.tableView.reloadData()
+                        self.tableView?.reloadData()
                     })
                 }())
             })
-            self.tableView.mj_header.endRefreshing()
+            self.tableView?.mj_header.endRefreshing()
         }())
     }
     
     func renderTableViewCell() {
         DispatchQueue.main.async(execute: {
-            self.tableView.reloadData()
+            self.tableView?.reloadData()
         })
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return (dataController?.videoDataSource?.count)! > 0 ? (dataController?.videoDataSource?.count)! : 0
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: kVideoCellReuseIdentifier, for: indexPath) as! DHVideoTableViewCell
         if (dataController?.videoDataSource?.count)! > 0 {
             let videoModel = dataController?.videoDataSource?[indexPath.row] as! DHVideoModel
@@ -55,11 +56,11 @@ class DHVideoViewController: UITableViewController {
         return cell
     }
     
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return kTutorialTableViewCellHeight
     }
     
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
@@ -68,15 +69,20 @@ class DHVideoViewController: UITableViewController {
         navigationController?.navigationBar.barTintColor = UIColor.black
         navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: kThemeColor]
         navigationItem.title = "掌刀"
+        self.automaticallyAdjustsScrollViewInsets = false
     }
     
     func setContentView() {
-        tableView.register(UINib(nibName: "DHVideoTableViewCell", bundle: nil), forCellReuseIdentifier: kVideoCellReuseIdentifier)
-        let menu: UISegmentedMenu = UISegmentedMenu(frame: CGRect(x: 0, y: 0, width: view.bounds.size.width, height: 50), contentDataSource: [""], titleDataSource: ["全部", "解说", "比赛", "明星", "趣味", "新手"], type: .fill)
+        let menu: UISegmentedMenu = UISegmentedMenu(frame: CGRect(x: 0, y: 60, width: view.bounds.size.width, height: 50), contentDataSource: [""], titleDataSource: ["全部", "解说", "比赛", "明星", "趣味", "新手"], type: .fill)
         view.addSubview(menu)
-        tableView.mj_header = MJRefreshNormalHeader(refreshingBlock: {
+        tableView = UITableView(frame: CGRect(x: 0, y: 60 + menu.bounds.size.height, width: view.bounds.size.width, height: view.bounds.size.height - 60 - menu.bounds.size.height), style: .plain)
+        tableView?.register(UINib(nibName: "DHVideoTableViewCell", bundle: nil), forCellReuseIdentifier: kVideoCellReuseIdentifier)
+        tableView?.mj_header = MJRefreshNormalHeader(refreshingBlock: {
             self.handleVideoData()
         })
+        view.addSubview(tableView!)
+        tableView?.delegate = self
+        tableView?.dataSource = self
     }
     
     override func viewDidLoad() {
