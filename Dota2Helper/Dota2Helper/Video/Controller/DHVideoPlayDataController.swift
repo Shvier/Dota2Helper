859c38e6b37fa8b148ee39fb13771deb9ea18fd8
Shvier <daulvi@hotmail.com>
2016-10-09 21:04:13 -0200

    Save

---
+++ Dota2Helper/Dota2Helper/Video/Controller/DHVideoPlayDataController.swift
@@ -0,0 +1,13 @@
+//
+//  DHVideoPlayDataController.swift
+//  Dota2Helper
+//
+//  Created by Shvier on 10/9/16.
+//  Copyright © 2016 Shvier. All rights reserved.
+//
+
+import UIKit
+
+class DHVideoPlayDataController: NSObject {
+
+}
---
+++ Dota2Helper/Dota2Helper/Video/Controller/DHVideoPlayViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DHVideoPlayViewController.swift
+//  Dota2Helper
+//
+//  Created by Shvier on 10/9/16.
+//  Copyright © 2016 Shvier. All rights reserved.
+//
+
+import UIKit
+
+class DHVideoPlayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
