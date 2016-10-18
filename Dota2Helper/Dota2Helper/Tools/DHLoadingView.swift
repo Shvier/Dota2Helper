61793a6ca5d6a2d6b34bf796593888ae93d162a8
Shvier <daulvi@hotmail.com>
2016-10-19 00:13:13 +0800

    Fix LoadingView layout

--- Dota2Helper/Dota2Helper/Tools/DHLoadingView.swift
+++ Dota2Helper/Dota2Helper/Tools/DHLoadingView.swift
@@ -23,11 +23,11 @@ class DHLoadingView: UIView {
         
         let hint: String = "正在加载"
         let hintLabel: UILabel = UILabel(frame: CGRect(x: 0, y: 0, width: hint.sizeOfContent(font: UIFont(name: "Helvetica", size: 20)!).width, height: 18))
-        hintLabel.center = CGPoint(x: frame.size.width/2, y: frame.size.height/2)
         hintLabel.text = hint
         hintLabel.textAlignment = .center
         let indicator: UIActivityIndicatorView = UIActivityIndicatorView(activityIndicatorStyle: .gray)
-        indicator.center = CGPoint(x: frame.size.width/2 - hintLabel.frame.size.width/2 - indicator.frame.size.width/2, y: frame.size.height/2)
+        hintLabel.center = CGPoint(x: frame.size.width/2 + hintLabel.frame.size.width/4 + indicator.frame.size.width/4, y: frame.size.height/2)
+        indicator.center = CGPoint(x: frame.size.width/2 - hintLabel.frame.size.width/2 + indicator.frame.size.width/2, y: frame.size.height/2)
         indicator.startAnimating()
         indicator.hidesWhenStopped = true
         self.addSubview(indicator)
