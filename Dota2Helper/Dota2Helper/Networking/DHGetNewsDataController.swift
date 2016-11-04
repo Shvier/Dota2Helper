369eb688e9b570623b8be6e152959a81f412f540
Shvier <daulvi@hotmail.com>
2016-11-04 11:38:57 +0800

    Fix Warnings

---
+++ Dota2Helper/Dota2Helper/Networking/DHGetNewsDataController.swift
@@ -0,0 +1,20 @@
+//
+//  DHGetNewsDataController.swift
+//  Dota2Helper
+//
+//  Created by Shvier on 04/11/2016.
+//  Copyright © 2016 Shvier. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class DHGetNewsDataController: NSObject {
+
+    static let sharedInstance = DHGetNewsDataController()
+    
+    func requestNewsData(success: @autoclosure @escaping () -> Void, failure: @autoclosure @escaping () -> Void) {
+    
+    }
+    
+}
