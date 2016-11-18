56cfe7f6d4b19edda8e06bbbb0d9e42df9264158
Shvier <daulvi@hotmail.com>
2016-11-18 23:59:48 +0800

    Update JournalCellDataController

--- Dota2Helper/Dota2Helper/Journal/DataController/DHJournalCellDataController.swift
+++ Dota2Helper/Dota2Helper/Journal/DataController/DHJournalCellDataController.swift
@@ -17,5 +17,9 @@ class DHJournalCellDataController: NSObject {
     func loadMoreUpates(nid: String, success: @escaping (_ response: Data) -> Void, failure: @autoclosure @escaping () -> Void) {
         DHLoadMoreUpdatesRequest.requestMoreUpdatesData(nid: nid, success: success, failure: failure)
     }
+    
+    func getUpdatesDetail(date: String, nid: String, success: @escaping (_ urlString: String) -> Void, failure: @autoclosure @escaping () -> Void) {
+        DHGetNewsDetailRequest.requestNewsDetailData(date: date, nid: nid, success: success, failure: failure)
+    }
         
 }
