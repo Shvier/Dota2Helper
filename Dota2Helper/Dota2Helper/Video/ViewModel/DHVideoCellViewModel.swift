57253536f8fe5b360ffb518b0107c8e0d8bc25b6
Shvier <daulvi@hotmail.com>
2016-11-22 22:23:57 +0800

    Add api for DHVideoCellViewModel

--- Dota2Helper/Dota2Helper/Video/ViewModel/DHVideoCellViewModel.swift
+++ Dota2Helper/Dota2Helper/Video/ViewModel/DHVideoCellViewModel.swift
@@ -30,4 +30,96 @@ class DHVideoCellViewModel: NSObject {
         self.date = videoModel?.date
     }
     
+    let dataController: DHVideoCellDataController = DHVideoCellDataController.sharedInstance
+    
+    func getAllVideos(success: @escaping(_ response: Data) -> Void, failure: @autoclosure @escaping () -> Void) {
+        dataController.getAllVideos(success: { [unowned self] (response) in
+            
+            }, failure: {} ())
+    }
+    
+    func getJieshuoVideos(success: @escaping(_ response: Data) -> Void, failure: @autoclosure @escaping () -> Void) {
+        dataController.getJieshuoVideos(success: { [unowned self] (response) in
+            
+            }, failure: {} ())
+    }
+    
+    func getBisaiVideos(success: @escaping(_ response: Data) -> Void, failure: @autoclosure @escaping () -> Void) {
+        dataController.getBisaiVideos(success: { [unowned self] (response) in
+            
+            }, failure: {} ())
+    }
+    
+    func getCelebrityVideos(success: @escaping(_ response: Data) -> Void, failure: @autoclosure @escaping () -> Void) {
+        dataController.getCelebrityVideos(success: { [unowned self] (response) in
+            
+            }, failure: {} ())
+    }
+    
+    func getQuweiVideos(success: @escaping(_ response: Data) -> Void, failure: @autoclosure @escaping () -> Void) {
+        dataController.getQuweiVideos(success: { [unowned self] (response) in
+            
+            }, failure: {} ())
+    }
+    
+    func getBeginnerVideos(success: @escaping(_ response: Data) -> Void, failure: @autoclosure @escaping () -> Void) {
+        dataController.getBeginnerVideos(success: { [unowned self] (response) in
+            
+            }, failure: {} ())
+    }
+    
+    func getAdvancedVideos(success: @escaping(_ response: Data) -> Void, failure: @autoclosure @escaping () -> Void) {
+        dataController.getAdvancedVideos(success: { [unowned self] (response) in
+            
+            }, failure: {} ())
+    }
+    
+    func loadMoreAllVideos(vid: String, success: @escaping(_ response: Data) -> Void, failure: @autoclosure @escaping () -> Void) {
+        dataController.loadMoreAllVideos(vid: vid, success: { [unowned self] (response) in
+            
+            }, failure: {} ())
+    }
+    
+    func loadMoreJieshuoVideos(vid: String, success: @escaping(_ response: Data) -> Void, failure: @autoclosure @escaping () -> Void) {
+        dataController.loadMoreJieshuoVideos(vid: vid, success: { [unowned self] (response) in
+            
+            }, failure: {} ())
+    }
+    
+    func loadMoreBisaiVideos(vid: String, success: @escaping(_ response: Data) -> Void, failure: @autoclosure @escaping () -> Void) {
+        dataController.loadMoreBisaiVideos(vid: vid, success: { [unowned self] (response) in
+            
+            }, failure: {} ())
+    }
+    
+    func loadMoreCelebrityVideos(vid: String, success: @escaping(_ repsonse: Data) -> Void, failure: @autoclosure @escaping () -> Void) {
+        dataController.loadMoreCelebrityVideos(vid: vid, success: { [unowned self] (response) in
+            
+            }, failure: {} ())
+    }
+    
+    func loadMoreQuweiVideos(vid: String, success: @escaping(_ response: Data) -> Void, failure: @autoclosure @escaping () -> Void) {
+        dataController.loadMoreQuweiVideos(vid: vid, success: { [unowned self] (response) in
+            
+            }, failure: {} ())
+    }
+    
+    func loadMoreBeginnerVideos(vid: String, success: @escaping(_ response: Data) -> Void, failure: @autoclosure @escaping () -> Void) {
+        dataController.loadMoreBeginnerVideos(vid: vid, success: { [unowned self] (response) in
+            
+            }, failure: {} ())
+    }
+    
+    func loadMoreAdvancedVideos(vid: String, success: @escaping(_ response: Data) -> Void, failure: @autoclosure @escaping () -> Void) {
+        dataController.loadMoreAdvancedVideos(vid: vid, success: { [unowned self] (response) in
+            
+            }, failure: {} ())
+    }
+    
+    func getRelatedVideos(vid: String, success: @escaping(_ response: Data) -> Void, failure: @autoclosure @escaping () -> Void) {
+        dataController.getRelatedVideos(vid: vid, success: { [unowned self] (response) in
+            
+            }, failure: {} ())
+    }
+    
 }
