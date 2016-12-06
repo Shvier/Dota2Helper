7bc4c3a3e25b1894bf3903ade84b2ece670e969a
Shvier <daulvi@hotmail.com>
2016-12-06 16:37:33 +0800

    Add guard else for reachability on DHVideoVC

--- Dota2Helper/Dota2Helper/Video/Controller/DHVideoViewController.swift
+++ Dota2Helper/Dota2Helper/Video/Controller/DHVideoViewController.swift
@@ -162,7 +162,9 @@ class DHVideoViewController: DHBaseViewController {
     
 // MARK: - Lift Cycle
     override func reachabilityChanged(note: NSNotification) {
-        let reachability = note.object as! Reachability
+        guard let reachability = note.object as? Reachability else {
+            return
+        }
         if reachability.isReachable {
             noNetworkView?.hide()
             handleVideoData()
